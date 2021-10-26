ff99855e97c35b1a2f255ac86d27a7d391e76481
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2021-10-26 12:46:22 +0900

    タグ機能の追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -10,7 +10,10 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
-  has_many :comments
+  # 親モデルを削除する際に、その親モデルに紐づく「子モデル」も一緒に削除できる
+  has_many :comments, dependent: :delete_all
+  has_many :board_tag_relations, dependent: :delete_all
+  has_many :tags, through: :board_tag_relations
   
   validates :name, presence: true, length: {maximum: 10}
   validates :title, presence: true, length: {maximum: 30}
